a8ed3f657d186f6b33faafe1b639214662442c52
ComPDF-Youna <youna@compdf.com>
2026-06-01 15:01:28 +0800

    V4.1.0 Update

---
+++ ComPDF_Conversion_windows_c/doc/html/dir_e96bb96152b64bd831bbb4683d882cfb.js
@@ -0,0 +1,7 @@
+var dir_e96bb96152b64bd831bbb4683d882cfb =
+[
+    [ "com_config_macros.h", "com__config__macros_8h.html", "com__config__macros_8h" ],
+    [ "compdf_basictypes_c.h", "compdf__basictypes__c_8h.html", "compdf__basictypes__c_8h" ],
+    [ "compdf_common_c.h", "compdf__common__c_8h.html", "compdf__common__c_8h" ],
+    [ "compdf_conversion_c.h", "compdf__conversion__c_8h.html", "compdf__conversion__c_8h" ]
+];
---
+++ ComPDF_Conversion_windows_c/doc/html/search/defines_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['compdfkit_5fdecl_0',['COMPDFKIT_DECL',['../com__config__macros_8h.html#a66389c9c33678081cfca79f8db24e922',1,'com_config_macros.h']]],
+  ['compdfkit_5fstring_1',['COMPDFKIT_STRING',['../compdf__basictypes__c_8h.html#a0de97e1e7f5825565f0882eeb464deb7',1,'compdf_basictypes_c.h']]],
+  ['csdk_5fcompdfkit_5fcall_2',['CSDK_COMPDFKIT_CALL',['../compdf__basictypes__c_8h.html#a532c1bb9b9764acb77005fdaba5b6a68',1,'compdf_basictypes_c.h']]]
+];
